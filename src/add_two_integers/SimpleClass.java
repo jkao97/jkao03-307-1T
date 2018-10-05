2c56b6944aeb62ea3a93ab1c41ec914dea0bd3d0
jkao97 <bleach94539@gmail.com>
2018-10-05 14:37:15 -0700

    travis test 1

---
+++ src/add_two_integers/SimpleClass.java
@@ -0,0 +1,13 @@
+package add_two_integers;
+
+public class SimpleClass {
+	public static void main(String[] args) {
+		//Check if code runs
+		int z = simpleMethod(5, 6);
+		System.out.println("Done!");
+	}
+	
+	public static int simpleMethod(int x, int y) {
+		return x + y;
+	}
+}
